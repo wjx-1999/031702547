38bc9e11d226896ba30f4cd43637b7c7d99ae739
wjx_1999 <1552902040@qq.com>
2019-09-26 15:32:00 +0800

    add match_name .py

--- 031702547.py
+++ 031702547.py
@@ -1,20 +1,18 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import re # 提供各种正则表达式的操作
-import cpca #用于提取简体中文字符串中省，市和区
 import json #将输出文件的格式转换为json
+import cpca #用于提取简体中文字符串中省，市和区
+import re # 提供各种正则表达式的操作
 
-def split_name(s: str):  # 将名字从字符串中提取出来
 
-
-
-def match_tel(s: str):  # Pick up phone number
-    tel = re.search('\d{11}', s).group(0)
-    match = re.split('\d{11}', s)
-    rest_addr = match[0] + match[1]  # join two string
-    return (tel, rest_addr)
-
+def match_name_tel(s: str):  # get name and phone number
+    match = s.split(",")
+    name = match[0] #get name
+    tel = re.search('\d{11}', match[1]).group(0) #get phone number
+    mat = re.split('\d{11}', match[1])
+    rest_addr = mat[0] + mat[1]  # join two string
+    return (name, tel, rest_addr)
 
 
 def solve_5(str0: str):
