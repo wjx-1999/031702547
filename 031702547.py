c7bc409aa1466fe99916c2d7437ebf8ca735f681
wjx_1999 <1552902040@qq.com>
2019-09-26 19:05:40 +0800

    add the rest .py

--- 031702547.py
+++ 031702547.py
@@ -17,7 +17,6 @@ def match_name_tel(s: str):  # get name and phone number
     tmp = dict(DataFrame.iloc[0, 0:4])
     return (name, tel, tmp)
 
-
 def detail_address(addr: str):
     town = village = street = road = alley = None  # init detail address
     if "镇" in addr:
@@ -41,22 +40,92 @@ def detail_address(addr: str):
         addr = addr.split("巷")[1]
     return (town, village, street, road, alley)
 
-
 def solve_5(str0: str):
+    name, phone, tmp = match_name_tel(str0)
+    addr1 = tmp['地址']  # get detail address
+    town, village, street, road, alley = detail_address(addr1)
 
+    addr = [tmp['省'], tmp['市'], tmp['区'], "", "", ]
+    if town != None:
+        addr[3] += (town)
+    if village != None:
+        addr[3] += (village)
+
+    if street != None:
+        addr[3] += (street)
+    if road != None:
+        addr[4] += (road)
+    if alley != None:
+        addr[4] += (alley)
+    if street == None:
+        if road == None:
+            if alley == None:
+                if town == None:
+                    if village == None:
+                        addr[4] = ""
+
+    addr[4] += addr1
+
+    info = {
+        "姓名": name,
+        "手机": phone,
+        "地址": addr
+    }
+    # 编码成json类型数据
+    info_data = json.dumps(info, ensure_ascii=False)
+    print(info_data)
+    # print(json.loads(info_data))#打印结果
 
 
 def solve_7(str0: str):
+    name, phone, tmp = match_name_tel(str0)
+    addr1 = tmp['地址']  # 提取出详细地址
+    town, village, street, road, alley = detail_address(addr1)
 
+    if re.search(r"\d*号", addr1) is None:
+        hao = None
+    else:
+        hao = re.search(r"\d*号", addr1).group(0)
+        addr1 = addr1.split("号")[1]
+
+    addr = [tmp['省'], tmp['市'], tmp['区'], "", "", "", ]
+    if town != None:
+        addr[3] += town
+    if village != None:
+        addr[3] += village
+    # print(addr)
+    if street != None:
+        if town is None and village is None:
+            addr[3] += street
+        else:
+            addr[4] += street
+    if road != None:
+        addr[4] += road
+    if alley != None:
+        addr[4] += alley
+    if hao != None:
+        addr[5] += hao
+
+    addr.append(addr1)
+
+    info = {
+        "姓名": name,
+        "手机": phone,
+        "地址": addr
+    }
+    # 编码成json类型数据
+    info_data = json.dumps(info, ensure_ascii=False)
+    print(info_data)
+    # print(json.loads(info_data))#打印结果
 
 
 str0 = input()
-level = str0.split("!")[0]
-level = int(level)
-str0 = str0.split("!")[1]
+str0 = re.split("!",str0)
+level = int(str0[0])
+s = str0[1]
 if level == 1:
-    solve_5(str0)
+    solve_5(s)
 if level == 2:
-    solve_7(str0)
+    solve_7(s)
 if level == 3:
-    solve_7(str0)
+    solve_7(s)
