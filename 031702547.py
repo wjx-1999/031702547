976f099035b5bdfd6e6a645054db1a65f960952e
wjx_1999 <1552902040@qq.com>
2019-09-26 17:01:53 +0800

    add detail address

--- 031702547.py
+++ 031702547.py
@@ -7,12 +7,39 @@ import re # 提供各种正则表达式的操作
 
 
 def match_name_tel(s: str):  # get name and phone number
+    s = s.rstrip(".")
     match = s.split(",")
     name = match[0] #get name
     tel = re.search('\d{11}', match[1]).group(0) #get phone number
     mat = re.split('\d{11}', match[1])
     rest_addr = mat[0] + mat[1]  # join two string
-    return (name, tel, rest_addr)
+    DataFrame = cpca.transform([rest_addr], cut=False, pos_sensitive=True)  # 必须使用列表才能映射
+    tmp = dict(DataFrame.iloc[0, 0:4])
+    return (name, tel, tmp)
+
+
+def detail_address(addr: str):
+    town = village = street = road = alley = None  # init detail address
+    if "镇" in addr:
+        town = addr.split("镇")[0] + "镇"
+        addr = addr.split("镇")[1]
+    if "乡" in addr:
+        village = addr.split("乡")[0] + "乡"
+        addr = addr.split("乡")[1]
+    if "街" in addr:
+        if "街道" in addr:
+            street = addr.split("街道")[0] + "街道"
+            addr = addr.split("街道")[1]
+        else:
+            street = addr.split("街")[0] + "街"
+            addr = addr.split("街")[1]
+    if "路" in addr:
+        road = addr.split("路")[0] + "路"
+        addr = addr.split("路")[1]
+    if "巷" in addr:
+        alley = addr.split("巷")[0] + "巷"
+        addr = addr.split("巷")[1]
+    return (town, village, street, road, alley)
 
 
 def solve_5(str0: str):
